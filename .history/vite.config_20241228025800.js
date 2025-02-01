b443c01625619db3aec99489a27d9f4d77346c6e
Henry <henryicecrown@gmail.com>
2025-02-01 02:47:28 -0300

    Configuración para GitHub Pages

--- .history/vite.config_20241228025800.js
+++ .history/vite.config_20241228025800.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/WeatherAPI-Call/",  // 👈 Ajusta esto con el nombre de tu repo
   server: {
     host: '0.0.0.0', // Permite que el servidor sea accesible desde cualquier dispositivo en la red local
     port: 3000, // El puerto que deseas usar, por defecto es 3000
   },
-});
+})
